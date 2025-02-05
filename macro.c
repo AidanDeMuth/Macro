4c5d72bbcee86ca5bc6e83d3617f4c075aaad1e2
AidanDeMuth <aidanspace87@gmail.com>
2025-02-04 22:26:21 -0500

    Playback

--- macro.c
+++ macro.c
@@ -8,7 +8,8 @@
 
 #include "macro.h"
 
-void main() {
+void 
+main() {
     // Get monitor refresh rate
     float hz = 0;
     DEVMODE scr_settings;
@@ -19,83 +20,113 @@ void main() {
         hz = MIN_REFRESH_RATE; // Just default to 60, should be safe
     }
 
-    float rate = SECOND / hz;
+    node_t *my_node = init_node(hz);
+    record_macro(my_node);
 
-    // List to store all points
+    sleep(5);
+    play_macro(my_node);
+} /* main() */
 
-    node_t *my_node = init_node();
+void
+record_macro(node_t *node) {
+    float rate = SECOND / node->hz;
 
-    // Create a clock 
-
-    clock_t before = clock();
+    // Start of execution time
+    clock_t start_time = clock();
+    
     int count = 0;
 
-    char a = 0;
+    // Var list
+    int frame_count = 0;
+
+    bool lclick = 0;
+    bool rclick = 0;
+    unsigned char keys[MAX_KB_HITS];
+    short num_keys = 0;
+
     while (1) {
-        bool l = 0;
-        bool r = 0;
 
         // Handle Clicks
+
         if (GetAsyncKeyState(VK_LBUTTON) & 0x8000) { // most significant
-            l = 1;
+            lclick = 1;
         }
-        if (GetAsyncKeyState(VK_RBUTTON) & 0x8000) { // least
-            r = 1;
+        if (GetAsyncKeyState(VK_RBUTTON) & 0x8000) {
+            rclick = 1;
         }
 
         // printf("%d %d", lclick, rclick);
 
         // Handle kb-hits
-        
-        char keys[MAX_KB_HITS];
-        short num_keys = 0;
         while (kbhit()) {
-            if (num_keys < MAX_KB_HITS) { // read them all anyways
+            if (num_keys < MAX_KB_HITS) {
                 keys[num_keys] = getch();
                 printf("%c", keys[num_keys]);
                 
                 ++num_keys;
             }
-            else {
-                getch();
+            else { 
+                getch(); // Flush
             }
-            printf("%d", count);
         }
+
 
         POINT pos = {0};
         GetCursorPos(&pos);
 
-        insert_frame(my_node, l, r, keys, num_keys, pos);
-
-        if (count == 10000) {
+        if (count == 40000) {
             break;
         }
-        count++;
-        printf("\n");
-    }
 
-    // Why not printing
+        // END OLD FRAME
+        // Add frame struct
+        // Reset fields for next loop
 
-    frame_t iterator = my_node->frames[0];
-    printf("EFJALSDFJAKLSDJLASKDJFASJDF");
-    for (int i = 0; i < 10000; i++) {
-        printf("%d %d %d ", iterator.lclick, iterator.rclick, iterator.num_hits);
-        for (int j = 0; j < iterator.num_hits; j++) {
-            printf("%c ", iterator.kb_hits[j]);
+        int curr_frame = get_frame(start_time, rate);
+        if (curr_frame > frame_count) {
+            insert_frame(node, lclick, rclick, keys, num_keys, pos, frame_count);
+
+            memset(keys, 0, sizeof(keys));
+            num_keys = 0;
+
+            lclick = 0;
+            rclick = 0;
+
+            frame_count = curr_frame;
         }
-        printf("%d %d \n", iterator.point.x, iterator.point.y);
-        iterator = my_node->frames[i];
+        count++;
+
+        /* 
+        // Test printing frames
+        for (int i = 0; i < num_keys; i++) {
+            printf("%c ", keys[i]);
+        }
+        printf("\n");
+        */
     }
 
-    clock_t after = clock();
-    clock_t diff = after - before;
-    clock_t total = diff * 1000 / CLOCKS_PER_SEC;
-
+    printf("Its / sec = %f\n", ((float) count / ((clock() - start_time))) * 1000);
     printf("Points: %d\n", count);
-    printf("%ld\n", total);
-} /* main() */
+} /* record_macro() */
 
-node_t *init_node() {
+void
+play_macro(node_t *node) {
+    // PLAYBACK SECTION
+    double delay = SECOND / node->hz;
+    clock_t frame_loop = clock();
+
+    for (int i = 0; i < node->size; i++) {
+        SetCursorPos(node->frames[i].point.x, node->frames[i].point.y);
+
+        double wait_time = ((double) clock() - (double) frame_loop + delay) * 1000000; // in nanoseconds 
+        printf("Wait time: %lf\n", wait_time);
+        nanosleep((const struct timespec[]) {{0, wait_time}}, NULL);
+        frame_loop = clock();
+    }
+}
+
+node_t 
+*init_node(float hz) {
     node_t *new_node = malloc(sizeof(node_t));
     assert(new_node);
 
@@ -104,23 +135,47 @@ node_t *init_node() {
 
     new_node->size = 0;
     new_node->capacity = INIT_CAPACITY;
+    new_node->hz = hz;
     
     return new_node;
 } /* init_node() */
 
-void insert_frame(node_t *node, bool l, bool r, char *keys, short num_keys, 
-                  POINT pos) {
+void 
+insert_frame(node_t *node, bool l, bool r, char *keys, short num_keys, 
+             POINT pos, int frame_num) {
     frame_t new_frame = {0};
     new_frame.lclick = l;
     new_frame.rclick = r;
-    new_frame.num_hits = num_keys;
+    new_frame.num_keys = num_keys;
     
     for (int i = 0; i < num_keys; i++) {
         new_frame.kb_hits[i] = keys[i];
     }
 
     new_frame.point = pos;
+    new_frame.frame_num = frame_num;
 
     node->frames[node->size] = new_frame;
     ++(node->size);
-}
+} /* insert_frame() */
+
+// Returns the FLOOR of the current frame
+inline int 
+get_frame(clock_t start_time, float rate) {
+    return (clock() - start_time) / rate;
+} /* get_frame() */
+
+void
+print_frame(node_t *node, int index) {
+    printf("%d - %d %d - %d %d - %hu - ", 
+            node->frames[index].frame_num,
+            node->frames[index].lclick,
+            node->frames[index].rclick,
+            node->frames[index].point.x,
+            node->frames[index].point.y,
+            node->frames[index].num_keys);
+    for (int i = 0; i < node->frames[index].num_keys; i++) {
+        printf("%c ", node->frames[index].kb_hits[i]);
+    }
+    printf("\n");
+} /* print_frame() */
--- macro.h
+++ macro.h
@@ -4,7 +4,7 @@
 #ifndef MACRO_H
 #define MACRO_H
 
-#define SECOND 1000
+#define SECOND 1000L
 #define MAX_KB_HITS 12
 
 #define MIN_REFRESH_RATE 30
@@ -19,19 +19,25 @@ typedef struct frame_t frame_t;
 struct frame_t {
     bool lclick; // 1/8
     bool rclick; // 1/8
-    short num_hits; // 2
-    char kb_hits[MAX_KB_HITS]; // 12
+    short num_keys; // 2
+    int frame_num; // 4
     POINT point; // 8
+    unsigned char kb_hits[MAX_KB_HITS]; // 12
 };
 
 struct node_t {
     frame_t *frames;
     int size;
     int capacity;
-}; 
+    int hz;
+};
 
-node_t *init_node();
+node_t *init_node(float hz);
+void record_macro(node_t *node);
+void play_macro(node_t *node);
 void insert_frame(node_t *node, bool l, bool r, char *keys, short num_keys, 
-                  POINT pos);
+                  POINT pos, int frame_num);
+inline int get_frame(clock_t start_time, float rate);
+void print_frame(node_t *node, int index);
 
 #endif
